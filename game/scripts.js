a953fecf16946d24c965e5bca41c9327253057c1
Sarah King <sarahking573@gmail.com>
2022-06-29 18:42:47 -0400

    base code for dino game

---
+++ game/scripts.js
@@ -0,0 +1,29 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+    if (dino.classList != "jump") {
+
+        dino.classList.add("jump");
+
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 300);
+    }
+    
+}
+
+let isAlive = setInterval(function () {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    // detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 130) {
+        alert("Game Over!")
+    }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+    jump();
+
+});
