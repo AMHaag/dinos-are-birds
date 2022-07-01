abac7328cd62a98c78aea459275c00cfecef8ff0
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-06-30 23:18:38 -0400

    Corrected homeRoutes code in 'controllers' folder in index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,12 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
-
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
