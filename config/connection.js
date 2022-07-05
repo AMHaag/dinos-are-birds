4bff4386d5f88593b3337c6182559efd6e1e7e40
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-07-05 19:57:40 -0400

    fixed connections file

--- config/connection.js
+++ config/connection.js
@@ -18,3 +18,11 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
+
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+
+// // create connection to our db
+
+module.exports = sequelize;
