6eba8e53207f47b4be427436ff0f2221ddd2bd43
Pina Gomet <pina@Pinas-MacBook-Pro.local>
2022-07-05 00:24:35 -0400

    relabeled code

---
+++ game/scripts.js
@@ -0,0 +1,38 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+let score = 0;
+
+
+
+function jump() {
+    if (dino.classList != "jump") {
+
+        dino.classList.add("jump");
+
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 400);
+    }
+    
+}
+
+let isAlive = setInterval(function () {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    // detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop > 120) {
+        alert("Game Over! Score: " + score);
+        
+        // TODO: post score to database
+        // TODO: bring the user to game over screen
+    }
+    else if (cactusLeft <= 0) {
+        score += 10;
+    }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+    jump();
+
+});
