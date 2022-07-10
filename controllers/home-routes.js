69060a8553063691add9f7601605ce5fba0df08c
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-07-10 13:51:14 -0400

    log in and logout functional, session logic in place.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', { loggedIn: req.session.loggedIn });
 });
 
 router.get('/login', (req, res) => {
@@ -14,19 +14,19 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/dashboard', (req, res) => {
-  res.render('highscore');
+  res.render('highscore', { loggedIn: req.session.loggedIn });
 });
 
 router.get('/gameover', (req, res) => {
-  res.render('gameover');
+  res.render('gameover', { loggedIn: req.session.loggedIn });
 });
 
 router.get('/profile', (req, res) => {
-  res.render('user-profile');
+  res.render('user-profile', { loggedIn: req.session.loggedIn });
 });
 
 router.get('/game', (req, res) => {
-  res.render('game');
+  res.render('game', { loggedIn: req.session.loggedIn });
 });
 
 // router.get('/',(req,res)=>{})
