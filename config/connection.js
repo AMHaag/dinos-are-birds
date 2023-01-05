fde43ef7d96dc708fbed472c18a58f47525de26b
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2023-01-04 21:55:52 -0500

    changed SQL Database to Railway

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,10 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    }
+    'mysql://root:eD5QTawbYbccfkbFCeiP@containers-us-west-37.railway.app:7648/railway'
   );
-}
 
 // const Sequelize = require('sequelize');
 
